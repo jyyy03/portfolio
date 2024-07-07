1378d4b656676029b787bf780096af64af39debe
Nora Tang <jingyu_tang@brown.edu>
2024-07-07 17:19:56 -0500

    added preloader + icons description

--- dist/index.e782d142.js
+++ dist/index.e782d142.js
@@ -1,12 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const loader = document.querySelector(".loader-container");
-    const content = document.querySelector(".content");
-    loader.addEventListener("animationend", function() {
-        document.body.classList.remove("no-scroll-preload");
-        document.body.style.overflow = "visible"; // Set overflow to visible
-        loader.style.display = "none";
-        content.classList.add("visible"); // Fade in the content
-    });
+window.addEventListener("load", function() {
+    document.getElementById("preloader").style.display = "none";
 });
 
 //# sourceMappingURL=index.e782d142.js.map
